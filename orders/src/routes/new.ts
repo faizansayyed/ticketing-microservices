7d524ca86e19a3fa18a0c6f6755b01d215fb9ca8
Faizan Sayyed <faizansayyed36@gmail.com>
2022-11-06 12:02:34 +0530

    Order Model Setup, Creating an Order Status Enum, Order Creation Logic, Convenience Document Methods

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@faizansayyedorg/common-v2';
+import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
+    res.send({});
+  }
+);
+
+export { router as newOrderRouter };
