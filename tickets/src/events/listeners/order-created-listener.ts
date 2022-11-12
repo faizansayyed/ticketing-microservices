706d89bfd7bcddde2c3edd4a8ab0b0819835b074
Faizan Sayyed <faizansayyed36@gmail.com>
2022-11-12 11:42:05 +0530

    reservice a ticketand implemented test for same, publiching while listening

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,36 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@faizansayyedorg/common-v2';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketCreatedPublisher } from '../publishers/ticket-created-publisher';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+    await new TicketCreatedPublisher(this.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    })
+
+    // ack the message
+    msg.ack();
+  }
+}
