52dbb1195ba2a3784604296c6fe06db84ca08290
Faizan Sayyed <faizansayyed36@gmail.com>
2022-11-17 10:07:41 +0530

    delaying job processing

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,13 +1,25 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@faizansayyedorg/common-v2";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
-    onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
 
+        await expirationQueue.add(
+            {
+                orderId: data.id
+            },
+            {
+                delay
+            }
+        )
+
+        msg.ack();
     }
 
 
