9f89574341dad7b0cca91cc22bfeec8a67e6f09a
Faizan Sayyed <faizansayyed36@gmail.com>
2022-11-18 09:42:18 +0530

    updated typo in common module

--- common/src/events/expiration-complete-event.ts
+++ common/src/events/expiration-complete-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from "./subjects";
 
 
-export interface ExpirationCompleteEvet {
+export interface ExpirationCompleteEvent {
     subject: Subjects.ExpirationComplete;
     data: {
         orderId: string;
